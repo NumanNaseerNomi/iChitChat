e5a89c105b86c8771143ae907fa52ac5422ca389
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-06-11 14:42:03 +0500

    Update users.php

--- users.php
+++ users.php
@@ -29,9 +29,7 @@
         <input type="text" placeholder="Enter name to search...">
         <button><i class="fas fa-search"></i></button>
       </div>
-      <div class="users-list">
-  
-      </div>
+      <div class="users-list"></div>
     </section>
   </div>
 
